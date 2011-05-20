c1fd939fff430c915f010bd3e0f0082e77359c28
jimikar@gmail.com <jimikar@gmail.com@e85cd2bf-d0ed-5c39-8532-09a06220c97d>
2011-05-20 12:12:19 +0000

    Separating each tabs content to their own controls.

---
+++ GameWindowRelocator/Views/AboutControl.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace GameWindowRelocator.Views
+{
+    partial class AboutControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aboutLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // aboutLabel
+            // 
+            this.aboutLabel.AutoSize = true;
+            this.aboutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.aboutLabel.Location = new System.Drawing.Point(49, 30);
+            this.aboutLabel.Name = "aboutLabel";
+            this.aboutLabel.Size = new System.Drawing.Size(179, 176);
+            this.aboutLabel.TabIndex = 1;
+            this.aboutLabel.Text = "Game Window Relocator\r\n\r\n v1.0.0\r\n\r\n\r\nLicenced under GPL v2\r\n\r\n\r\nCreated by Jimi " +
+                "C\r\n\r\nMay 2011\r\n";
+            this.aboutLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // AboutControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.aboutLabel);
+            this.Name = "AboutControl";
+            this.Size = new System.Drawing.Size(276, 236);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label aboutLabel;
+    }
+}
---
+++ GameWindowRelocator/Views/SettingsControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Globalization;
+using System.Security;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace GameWindowRelocator.Views
+{
+    public partial class SettingsControl : UserControl
+    {
+        private const string StartupRegistryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+        
+        public SettingsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            // Run at system startup
+            RegistryKey rk = null;
+            try
+            {
+                rk = Registry.CurrentUser.OpenSubKey(StartupRegistryKey, true);
+            }
+            catch (SecurityException ex)
+            {
+                throw new SecurityException(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                throw new UnauthorizedAccessException(ex.Message);
+            }
+
+            if (rk == null)
+            {
+                // No writing rights
+                startWithWindowsCheckBox.Checked = false;
+                startWithWindowsCheckBox.Enabled = false;
+            }
+            else
+            {
+                // Run at startup ?
+                startWithWindowsCheckBox.Checked = (rk.GetValue("GameWindowRelocator") != null);
+            }
+
+            enableAutoRelacationChechBox.Checked = Properties.Settings.Default.EnableAutomaticRelocation;
+
+            timeIntervalNUD.Value = Properties.Settings.Default.AutomaticRelocationInterval;
+        }
+
+        private void startWithWindowsCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // Run at startup
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey(StartupRegistryKey, true);
+            if (startWithWindowsCheckBox.Checked)
+            {
+                rk.SetValue("GameWindowRelocator",
+                    String.Format(CultureInfo.CurrentCulture, "\"{0}\" {1}",
+                    Application.ExecutablePath.ToString(), "-startMinimized"));
+            }
+            else
+            {
+                rk.DeleteValue("GameWindowRelocator", false);
+            }
+        }
+        
+        private void enableAutoRelacationChechBox_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.EnableAutomaticRelocation = enableAutoRelacationChechBox.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void timeIntervalNUD_ValueChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.AutomaticRelocationInterval = (byte)timeIntervalNUD.Value;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
