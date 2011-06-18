05817998faae9d39129461319c8960b572033923
jimikar@gmail.com <jimikar@gmail.com@e85cd2bf-d0ed-5c39-8532-09a06220c97d>
2011-06-18 17:14:27 +0000

    Improving the "About" information window.

--- trunk/GameWindowRelocator/Views/AboutControl.cs
+++ trunk/GameWindowRelocator/Views/AboutControl.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
 
 namespace GameWindowRelocator.Views
 {
@@ -10,6 +13,18 @@ namespace GameWindowRelocator.Views
         public AboutControl()
         {
             InitializeComponent();
+        }
+
+        private void AboutControl_Load(object sender, System.EventArgs e)
+        {
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            versionLabel.Text = String.Format("Version {0}.{1}.{2}",
+                version.Major, version.Minor, version.Build);
+        }
+
+        private void linkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://code.google.com/p/game-window-relocator/");
         }
     }
 }
