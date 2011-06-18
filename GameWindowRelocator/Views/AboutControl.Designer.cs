ad649b0f8785c0f1fd618ba75568ef9124a597a4
jimikar@gmail.com <jimikar@gmail.com@e85cd2bf-d0ed-5c39-8532-09a06220c97d>
2011-06-18 17:14:27 +0000

    Improving the "About" information window.

--- GameWindowRelocator/Views/AboutControl.Designer.cs
+++ GameWindowRelocator/Views/AboutControl.Designer.cs
@@ -29,27 +29,53 @@
         private void InitializeComponent()
         {
             this.aboutLabel = new System.Windows.Forms.Label();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // aboutLabel
             // 
             this.aboutLabel.AutoSize = true;
-            this.aboutLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.aboutLabel.Location = new System.Drawing.Point(22, 30);
+            this.aboutLabel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.aboutLabel.Location = new System.Drawing.Point(33, 22);
             this.aboutLabel.Name = "aboutLabel";
-            this.aboutLabel.Size = new System.Drawing.Size(232, 176);
+            this.aboutLabel.Size = new System.Drawing.Size(211, 192);
             this.aboutLabel.TabIndex = 1;
-            this.aboutLabel.Text = "Game Window Relocator\r\n\r\n v1.0.0\r\n\r\nLicenced under GPL v2\r\n\r\nBased on code create" +
-                "d by\r\nthe EVEMon Development Team\r\n\r\nCreated by Jimi C\r\nMay 2011\r\n";
-            this.aboutLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.aboutLabel.Text = "Game Window Relocator\r\n\r\n\r\n\r\n© 2011, Jimi C\r\n\r\n\r\n\r\nLicenced under GPL v2\r\n\r\nBased" +
+                " on code created by\r\nthe EVEMon Development Team\r\n";
+            this.aboutLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.versionLabel.Location = new System.Drawing.Point(97, 56);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(82, 13);
+            this.versionLabel.TabIndex = 2;
+            this.versionLabel.Text = "Version 1.0.0";
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.Location = new System.Drawing.Point(13, 117);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(250, 13);
+            this.linkLabel.TabIndex = 3;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "http://code.google.com/p/game-window-relocator/";
+            this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
             // 
             // AboutControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.linkLabel);
+            this.Controls.Add(this.versionLabel);
             this.Controls.Add(this.aboutLabel);
             this.Name = "AboutControl";
             this.Size = new System.Drawing.Size(276, 236);
+            this.Load += new System.EventHandler(this.AboutControl_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +84,7 @@
         #endregion
 
         private System.Windows.Forms.Label aboutLabel;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.LinkLabel linkLabel;
     }
 }
